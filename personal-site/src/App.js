0734702a1ddf626781f21a1577d1f5364b09208d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2020-11-08 10:17:30 -0600

    Update site

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,36 +1,56 @@
-import './App.css';
+import "./App.css";
 
-import { GitHub, LinkedIn } from '@material-ui/icons'
-import { Button, Typography } from '@material-ui/core';
+import { GitHub, LinkedIn } from "@material-ui/icons";
+import { Button, Typography } from "@material-ui/core";
+
+const { REACT_COMMIT_HASH = "e0292a3f0ea67ad62759b1f482a9ecd0f310150e" } = process.env;
 
 function App() {
-
-  const openLink = url => {
-    window.open(url, '_blank');
-  }
-
-
   return (
     <div className="app">
       <div className="content-wrap">
-        <img width="100" id="profile-pic" src="me_irl.png" alt="Me in real life" />
+        <img
+          width="100"
+          id="profile-pic"
+          src="me_irl.png"
+          alt="Me in real life"
+        />
 
-        <div className="about-wrap">  
-          <Typography variant="h4">
-          Sean Boult
-          </Typography>
-          <Typography variant="body2">
-            I like to build things
-          </Typography>
+        <div className="about-wrap">
+          <Typography variant="h4">Sean Boult</Typography>
+          <Typography variant="body2">I like to build things 🛠️</Typography>
         </div>
 
         <div className="social-wrap">
-          <Button onClick={() => openLink('https://github.com/Hacksore')}>
+          <Button
+            title="My Github"
+            size="large"
+            onClick={() => window.open("https://github.com/Hacksore", "_blank")}
+          >
             <GitHub />
           </Button>
-          <Button onClick={() => openLink('https://www.linkedin.com/in/seanboult')}>
+          <Button
+            title="My Linkedin"
+            size="large"
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/seanboult", "_blank")
+            }
+          >
             <LinkedIn />
           </Button>
+        </div>
+
+        <div>
+          <Typography>
+            Built via commit{" "}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href={`https://github.com/Hacksore/Hacksore/commit/${REACT_COMMIT_HASH}`}
+            >
+              {REACT_COMMIT_HASH.substr(0, 7)}
+            </a>
+          </Typography>
         </div>
       </div>
     </div>
--- personal-site/src/App.test.js
+++ personal-site/src/App.test.js
@@ -1,10 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-
-  
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test("renders my name", () => {
+  render(<App />);
+  const nameElement = screen.getByText(/Sean Boult/i);
+  expect(nameElement).toBeInTheDocument();
 });
