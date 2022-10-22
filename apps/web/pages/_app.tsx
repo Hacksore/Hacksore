8670979d634c6fddba6492a4b75848a1036ce04c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-10-22 18:17:47 +0000

    More fixes

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,37 @@
+import Head from "next/head";
+
 interface AppProps {
   Component: any;
   pageProps: any;
 }
 
+const META_INFO = {
+  title: "Sean Boult - Personal Site",
+  description: "Full stack developer who likes ReactJS. You will find most if not all of my socials here.",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Sean {`"Hacksore"`} Boult</title>
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        <meta name="title" content={META_INFO.title}></meta>
+        <meta name="description" content={META_INFO.description}></meta>
+
+        <meta property="og:title" content={META_INFO.title}></meta>
+        <meta property="og:description" content={META_INFO.description}></meta>
+        <meta property="og:image" content="https://boult.me/img/banner.png"></meta>
+        <meta property="og:type" content="website"></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content={META_INFO.title}></meta>
+        <meta property="twitter:description" content={META_INFO.description}></meta>
+        <meta property="twitter:image" content="https://boult.me/img/banner.png"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -9,8 +9,6 @@ import { theme } from "theme";
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-import Head from "next/head";
-
 // setup firebase
 import "../firebaseConfig";
 
@@ -24,33 +22,11 @@ const StyledBox = styled(Box)(() => ({
   },
 }));
 
-const META_INFO = {
-  title: "Sean Boult - Personal Site",
-  description: "Full stack developer who likes ReactJS. You will find most if not all of my socials here.",
-};
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Head>
-        <title>Sean {`"Hacksore"`} Boult</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-        <meta name="title" content={META_INFO.title}></meta>
-        <meta name="description" content={META_INFO.description}></meta>
-
-        <meta property="og:title" content={META_INFO.title}></meta>
-        <meta property="og:description" content={META_INFO.description}></meta>
-        <meta property="og:image" content="https://boult.me/img/banner.png"></meta>
-        <meta property="og:type" content="website"></meta>
-
-        <meta property="twitter:card" content="summary_large_image"></meta>
-        <meta property="twitter:title" content={META_INFO.title}></meta>
-        <meta property="twitter:description" content={META_INFO.description}></meta>
-        <meta property="twitter:image" content="https://boult.me/img/banner.png"></meta>
-      </Head>
-
       <StyledBox>
         <div className="content-wrap">
           <About />
