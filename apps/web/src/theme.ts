0c7a0ce803a9b6fdf048a5f566d86d811557c116
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-11-03 02:49:15 +0000

    Fix lint

--- apps/web/src/theme.ts
+++ apps/web/src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme, lighten } from "@mui/material";
+import { createTheme } from "@mui/material";
 
 declare module "@mui/material/styles" {
   /* eslint-disable-next-line no-unused-vars */
