7cd9e21716a00b4ad5e6bbb107c165a620028166
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-03-19 16:18:24 -0500

    Add input to top

--- apps/web/src/pages/guestbook/index.tsx
+++ apps/web/src/pages/guestbook/index.tsx
@@ -1,5 +1,6 @@
 import { Box, styled, Typography } from "@mui/material";
 import GiscusComponent from "@giscus/react";
+import Head from "next/head";
 
 const StyledBox = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down("md")]: {
@@ -13,7 +14,12 @@ const StyledBox = styled(Box)(({ theme }) => ({
 function GuestBook() {
   return (
     <StyledBox>
-      <Typography variant="h2" sx={{ mt: 4, mb: 4, fontWeight: "bold" }}>Guestbook</Typography>
+      <Head>
+        <title>Sean Boult&apos;s Guestbook</title>
+      </Head>
+      <Typography variant="h2" sx={{ mt: 4, mb: 4, fontWeight: "bold" }}>
+        Guestbook
+      </Typography>
       <Box sx={{ width: "100%" }}>
         <GiscusComponent
           repo="Hacksore/Hacksore"
@@ -24,7 +30,7 @@ function GuestBook() {
           mapping="title"
           reactionsEnabled="1"
           emitMetadata="0"
-          inputPosition="bottom"
+          inputPosition="top"
           lang="en"
         />
       </Box>
