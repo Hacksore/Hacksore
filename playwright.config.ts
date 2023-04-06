40df64acf29bfe968357358c330359d9f86eb3c5
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-04-05 21:54:01 -0500

    Update deps

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@playwright/test";
-export default defineConfig({
-  use: {
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: "on-first-retry",
-  },
-});
