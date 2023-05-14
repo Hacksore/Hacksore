b2402968422890b9055a048f8b5e368a9cc64c93
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-14 12:12:00 -0500

    Refactor

--- apps/web/src/components/lightbox.tsx
+++ apps/web/src/components/lightbox.tsx
@@ -1,36 +1,12 @@
-import { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 // TODO: figure out migation to next/image
 import Image from "next/legacy/image";
-
-const imageSizes = [16, 32, 48, 64, 96, 128, 256, 384];
-const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
-
-// NOTE: I don;t think i need this if i don;t use the zoom plug
-function nextImageUrl(src: string, size: number) {
-  return `/_next/image?url=${encodeURIComponent(src)}&w=${size}&q=75`;
-}
+import { useState } from "react";
 
 export const CustomLightbox = ({ images }: { images: any[] }) => {
   const [open, setOpen] = useState(false);
-  // TODO: type this shit
-  const slides = images.map(({ src, width, height, alt }: any) => ({
-    width,
-    height,
-    src: nextImageUrl(src, width),
-    alt,
-    srcSet: imageSizes
-      .concat(...deviceSizes)
-      .filter(size => size <= width)
-      .map(size => ({
-        src: nextImageUrl(src, size),
-        width: size,
-        height: Math.round((height / width) * size),
-      })),
-  }));
-
-  const firstImage = slides[0];
+  const firstImage = images[0];
   return (
     <>
       <Image
@@ -41,7 +17,7 @@ export const CustomLightbox = ({ images }: { images: any[] }) => {
         layout="responsive"
         onClick={() => setOpen(true)}
       />
-      <Lightbox open={open} close={() => setOpen(false)} slides={slides} />
+      <Lightbox open={open} close={() => setOpen(false)} slides={images} />
     </>
   );
 };
