2408d573c5010a8126519aa47fc29c4ed5893ce3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-15 14:07:26 -0500

    Go back to legacy image for now

--- apps/web/src/components/lightbox.tsx
+++ apps/web/src/components/lightbox.tsx
@@ -1,44 +1,23 @@
 import Lightbox from "yet-another-react-lightbox";
-import zoomPlugin from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
 
-import Image from "next/image";
+// TODO: don't use the legacy image but figure out how to make
+import Image from "next/legacy/image";
 import { useState } from "react";
-
-const imageSizes = [16, 32, 48, 64, 96, 128, 256, 384];
-const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
-
-function nextImageUrl(src: any, size: any) {
-  return `/_next/image?url=${encodeURIComponent(src)}&w=${size}&q=75`;
-}
 
 export const CustomLightbox = ({ images }: { images: any[] }) => {
   const [open, setOpen] = useState(false);
   const firstImage = images[0];
-
-  const slides = images.map(({ src, width, height, alt }: any) => ({
-    width,
-    height,
-    src: nextImageUrl(src, width),
-    alt,
-    srcSet: imageSizes
-      .concat(...deviceSizes)
-      .filter(size => size <= width)
-      .map(size => ({
-        src: nextImageUrl(src, size),
-        width: size,
-        height: Math.round((height / width) * size),
-      })),
-  }));
 
   return (
     <>
       <Image
         src={firstImage}
         alt="Testing"
+        layout="responsive"
         onClick={() => setOpen(true)}
       />
-      <Lightbox open={open} close={() => setOpen(false)} slides={slides} plugins={[zoomPlugin]} />
+      <Lightbox open={open} close={() => setOpen(false)} slides={images} />
     </>
   );
 };
