45e7d10824a6e48fdab2cb462d7634b631b4a362
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-05-15 13:46:38 -0500

    No git lfs

--- apps/web/src/components/lightbox.tsx
+++ apps/web/src/components/lightbox.tsx
@@ -1,8 +1,8 @@
 import Lightbox from "yet-another-react-lightbox";
 import zoomPlugin from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/styles.css";
-// TODO: figure out migration to next/image
-import Image from "next/legacy/image";
+
+import Image from "next/image";
 import { useState } from "react";
 
 const imageSizes = [16, 32, 48, 64, 96, 128, 256, 384];
@@ -34,11 +34,8 @@ export const CustomLightbox = ({ images }: { images: any[] }) => {
   return (
     <>
       <Image
-        src={firstImage.src}
-        alt={firstImage.alt}
-        width={firstImage.width}
-        height={firstImage.height}
-        layout="responsive"
+        src={firstImage}
+        alt="Testing"
         onClick={() => setOpen(true)}
       />
       <Lightbox open={open} close={() => setOpen(false)} slides={slides} plugins={[zoomPlugin]} />
