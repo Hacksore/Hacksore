8ccf2e43ebf625737f6251c49f4974686d4d2429
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-17 10:05:56 -0500

    Ignore warning

--- apps/bot/src/types.d.ts
+++ apps/bot/src/types.d.ts
@@ -2,5 +2,6 @@ import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
 export interface CommandInt {
   data: SlashCommandBuilder | any;
+  // eslint-disable-next-line 
   execute: (interaction: CommandInteraction) => Promise<void>;
 }
