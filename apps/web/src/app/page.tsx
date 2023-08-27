7164347b3c3e37201bf3433116d3b5b8b23a3533
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-27 11:23:46 -0500

    Fix meta

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,6 +3,29 @@ import { Projects } from "../components/projects";
 import { About } from "../components/about";
 import Link from "next/link";
 import { Button } from "@mui/material";
+import { Metadata } from "next";
+
+const META_INFO = {
+  title: "Sean Boult - Personal Site",
+  description: "Full Stack Developer Who Likes React",
+};
+
+export const metadata: Metadata = {
+  title: META_INFO.title,
+  description: META_INFO.description,
+  openGraph: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: [ "/img/og-image.png" ],
+    type: "website",
+  },
+  twitter: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: [ "/img/og-image.png" ],
+    card: "summary_large_image",
+  },
+};
 
 function App() {
   return (
@@ -16,32 +39,17 @@ function App() {
 
       <section className="linkys">
         <Link style={{ textDecoration: "none" }} href="/guestbook">
-          <Button
-            sx={{ textTransform: "lowercase" }}
-            className="link"
-            color="secondary"
-            variant="contained"
-          >
+          <Button sx={{ textTransform: "lowercase" }} className="link" color="secondary" variant="contained">
             ✍️ /guestbook
           </Button>
         </Link>
         <Link style={{ textDecoration: "none" }} href="/uses">
-          <Button
-            sx={{ textTransform: "lowercase" }}
-            className="link"
-            color="secondary"
-            variant="contained"
-          >
+          <Button sx={{ textTransform: "lowercase" }} className="link" color="secondary" variant="contained">
             💊 /uses
           </Button>
         </Link>
         <Link style={{ textDecoration: "none" }} href="/keyboard">
-          <Button
-            sx={{ textTransform: "lowercase" }}
-            className="link"
-            color="secondary"
-            variant="contained"
-          >
+          <Button sx={{ textTransform: "lowercase" }} className="link" color="secondary" variant="contained">
             ⌨️ /keyboard
           </Button>
         </Link>
