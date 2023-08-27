b74ba5b8b783b38e5d485f157bf8b0517b31388b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-27 11:42:11 -0500

    Fix meta

--- apps/web/src/app/uses/page.tsx
+++ apps/web/src/app/uses/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
 import Uses from "./uses-client"
+
+export const metadata: Metadata = {
+  title: "Uses",
+  description: "Most of the things I use",
+};
+
 export default Uses;
 
