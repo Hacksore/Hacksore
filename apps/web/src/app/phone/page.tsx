9a07e8bc5536bf267a50b32f1578749b423eb5c1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-23 17:06:00 -0500

    Fix og

--- apps/web/src/app/phone/page.tsx
+++ apps/web/src/app/phone/page.tsx
@@ -1,9 +1,26 @@
 import { Metadata } from "next";
 import { Phone } from "./phone-client";
 
-export const metadata: Metadata = {
+const META_INFO = {
   title: "iPhone GOATED loadout",
-  description: "The absolutely most goated phone loadout in the world",
+  description: "The absolutely most goated phone loadout in the world"
+};
+
+export const metadata: Metadata = {
+  title: META_INFO.title,
+  description: META_INFO.description,
+  openGraph: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: ["/img/goated-iphone.png"],
+    type: "website",
+  },
+  twitter: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: ["/img/goated-iphone.png"],
+    card: "summary_large_image",
+  },
 };
 
 export default function Keybaord() {
