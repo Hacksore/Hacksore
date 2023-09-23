b4779bb31df7fd227127f29e99a6919e05df8d5f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-23 16:54:55 -0500

    Add goat pic

--- apps/web/src/app/keyboard/page.tsx
+++ apps/web/src/app/keyboard/page.tsx
@@ -1,9 +1,26 @@
 import { Metadata } from "next";
 import { KeyboardMd } from "./keyboard-client";
 
+const META_INFO = {
+  title: "iPhone GOATED loadout",
+  description: "The absolutely most goated phone loadout in the world"
+};
+
 export const metadata: Metadata = {
-  title: "Keyboard",
-  description: "My journey to learn the ZSA moonlander keyboard",
+  title: META_INFO.title,
+  description: META_INFO.description,
+  openGraph: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: ["/img/goated-iphone.png"],
+    type: "website",
+  },
+  twitter: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    images: ["/img/goated-iphone.png"],
+    card: "summary_large_image",
+  },
 };
 
 export default function Keybaord() {
