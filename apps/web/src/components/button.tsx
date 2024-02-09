e89b4cd543118eec2b02612257f51edb0d084f3a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-09 08:14:20 -0600

    Change blue

--- apps/web/src/components/button.tsx
+++ apps/web/src/components/button.tsx
@@ -1,7 +1,7 @@
 export const Button = ({ children, ariaLabel, ...props }: { ariaLabel: string; children: JSX.Element | string }) => (
   <button
     aria-label={ariaLabel}
-    className="uppercase min-w-[44px] h-[42px] text-center bg-[#20262D] hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+    className="uppercase min-w-[44px] h-[42px] text-center bg-[#20262D] hover:bg-blue-600 text-white font-bold py-1 px-2 rounded"
     {...props}
   >
     {children}
