f4cfeaffa969013628fe26e1086f62dfa2bdc08a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-14 11:57:05 -0500

    Use top level env

--- apps/bot/src/env.ts
+++ apps/bot/src/env.ts
@@ -4,15 +4,15 @@ const envVariables = z.object({
   /**
    * The github access token
    */
-  GITHUB_ACCESS_TOKEN: z.string().nonempty(),
+  GITHUB_ACCESS_TOKEN: z.string().min(1),
   /**
    * The Discord bot token
    */
-  DISCORD_TOKEN: z.string().nonempty(),
+  DISCORD_TOKEN: z.string().min(1),
   /**
    * The json service account key for firebase admin as a base64 string
    */
-  FIREBASE_SA_BASE64: z.string().nonempty(),
+  FIREBASE_SA_BASE64: z.string().min(1),
 });
 
 // parse the environment variables
