477a0a46c72ae8480097b778051008f67c91ea2c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-03-20 15:41:20 -0500

    🙅♂️cape

---
+++ apps/web/src/app/cape/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import { Cape } from "./cape.client";
+
+const META_INFO = {
+  title: "Hacksore's Minecraft Capes",
+  description: "Yes I have a 2011 Minecraft cape and it's not for sale",
+};
+
+export const metadata: Metadata = {
+  title: META_INFO.title,
+  description: META_INFO.description,
+  openGraph: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+    type: "website",
+  },
+  twitter: {
+    title: META_INFO.title,
+    description: META_INFO.description,
+  },
+};
+
+export default function Keybaord() {
+  return (
+    <div>
+      <Cape />
+    </div>
+  );
+}
