09f597a54c5c87c03dca1d243df2abcd8eca3e08
Sean Boult <996134+Hacksore@users.noreply.github.com>
2026-04-15 21:24:02 -0500

    Shrimple home

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,4 @@
-import { ReactSkinview3d } from "react-skinview3d";
-import { useTexturesLoaded } from "../hooks/use-textures-loaded";
-
 export const About = () => {
-  const areTexturesLoaded = useTexturesLoaded(["/img/Hacksore.png", "/img/cape-2011.png"]);
-
   return (
     <div className="flex flex-col text-center items-center gap-2">
       <div className="flex flex-col items-center gap-2">
@@ -11,26 +6,7 @@ export const About = () => {
           Sean Boult
         </p>
       </div>
-
-      <p>Full Stack Developer Who Likes React</p>
-
-      <div className="w-[200px] h-[200px] relative">
-        {!areTexturesLoaded && (
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="w-8 h-8 border-4 border-gray-600 border-t-white rounded-full animate-spin" />
-          </div>
-        )}
-        <ReactSkinview3d
-          width={200}
-          height={200}
-          skinUrl="/img/Hacksore.png"
-          capeUrl="/img/cape-2011.png"
-          onReady={({ viewer }) => {
-            viewer.controls.enableZoom = false;
-            viewer.autoRotate = true;
-          }}
-        />
-      </div>
+      <p className="text-2xl">Full Stack developer who likes React</p>
     </div>
   );
 };
